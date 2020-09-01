17dd40acc3fa64e4ec533a12682d4bdc5be4473d
chanul94 <cksdnfwkd@naver.com>
2020-09-02 00:53:51 +0900

    "numsgame"_Implement computerMode room

--- numsgame/app.js
+++ numsgame/app.js
@@ -31,19 +31,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Run when client connects
 io.on('connection', socket => {
   socket.on('joinRoom', ({ username, room, avatarId }) => {
+    if (room === undefined) room = username;
     const user = userJoin(socket.id, username, room, avatarId);
   
     socket.join(user.room);
     console.log(getRoomUsers(user.room));
 
-    setTimeout(() => {
-      let roomUsers = getRoomUsers(user.room);
-      if(roomUsers.length !== 2){
-        io.to(user.room).emit('connectError');
-        console.log('disconnect');
-        socket.disconnect();
-      }
-    }, 1000)
+    // setTimeout(() => {
+    //   let roomUsers = getRoomUsers(user.room);
+    //   if(roomUsers.length !== 2){
+    //     io.to(user.room).emit('connectError');
+    //     console.log('disconnect');
+    //     socket.disconnect();
+    //   }
+    // }, 1000)
 
     // io.to(user.room).emit('message', `${username}님이 들어오셨습니다`);
 
@@ -57,6 +58,7 @@ io.on('connection', socket => {
       } 
 
       roomNum[user.room].ans = randomNum();
+      console.log('roomNum[user.room].ans: ', roomNum[user.room].ans);
       // io.to(user.room).emit('start', {
       //   message: roomNum[user.room]
       // });
@@ -121,60 +123,62 @@ io.on('connection', socket => {
   })
   //숫자 입력
   socket.on('submit', ({username, room, arr}) => {
-    // console.log({username, arr});
     let user = getCurrentUser(socket.id);
     let users = getRoomUsers(user.room);
-
     let rival;
-    if(users[0].username === user.username){
-      rival = users[1].username;
-    } else {
-      rival = users[0].username;
-    }
-
-    if(getRoomUsers(room)[roomNum[room].turn].username === username){
-      let res = getResult(roomNum[room].ans, arr);
-      console.log(roomNum[room].ans);
-      // console.log(res);
-      // io.to(room).emit('message', `${username}님이 ${arr.join('')}을 입력했습니다`);
-      // io.to(room).emit('message', `결과는 ${res} 입니다`);
-
-      io.to(room).emit('res', {username: username, num: arr.join(''), res: res});
-      let data ={arr:arr.join(''), res: res};
-      let log = makelog(room, username, data);
-      // console.log(log);
-
-      if(log[getRoomUsers(user.room)[0].username].length === 5 && log[getRoomUsers(user.room)[1].username].length === 5){
-        // io.to(room).emit('end', null);
-        endAll(username, rival, room,io, true);
-        // console.log('draw');
-      }
-
-      else if(res === '4S0B'){
-        // io.to(room).emit('message', `${username}님이 승리하셨습니다`);
-        // io.to(room).emit('end', username);
-        endAll(username, rival, room, io);
-        // io.to(room).emit('end', {winner: username, answer: roomNum[room].ans});
+    try{
+      if(users[0].username === user.username){
+        rival = users[1].username;
       } else {
-        !roomNum[room].turn ? roomNum[room].turn = 1 : roomNum[room].turn = 0;
-        io.to(user.room).emit('turn', getRoomUsers(user.room)[roomNum[user.room].turn].username);
+        rival = users[0].username;
       }
-      // io.to(room).emit('score', {username: username, result: res, enter: arr.join('')}); //전광판
-
-      
-    } else {
-      socket.emit('message', `당신차례가 아닙니다`)
+  
+      // 컴퓨터모드일 경우 방이름
+      if(room === undefined) room = user.room;
+  
+      if(getRoomUsers(room)[roomNum[room].turn].username === username){
+        let res = getResult(roomNum[room].ans, arr);
+        // console.log(res);
+        // io.to(room).emit('message', `${username}님이 ${arr.join('')}을 입력했습니다`);
+        // io.to(room).emit('message', `결과는 ${res} 입니다`);
+  
+        io.to(room).emit('res', {username: username, num: arr.join(''), res: res});
+        let data = {arr:arr.join(''), res: res};
+        let log = makelog(room, username, data);
+        // console.log(log);
+  
+        if(log[getRoomUsers(user.room)[0].username].length === 5 && log[getRoomUsers(user.room)[1].username].length === 5){
+          // io.to(room).emit('end', null);
+          endAll(username, rival, room,io, true);
+          // console.log('draw');
+        }
+  
+        else if(res === '4S0B'){
+          // io.to(room).emit('message', `${username}님이 승리하셨습니다`);
+          // io.to(room).emit('end', username);
+          endAll(username, rival, room, io);
+          // io.to(room).emit('end', {winner: username, answer: roomNum[room].ans});
+        } else {
+          !roomNum[room].turn ? roomNum[room].turn = 1 : roomNum[room].turn = 0;
+          io.to(user.room).emit('turn', getRoomUsers(user.room)[roomNum[user.room].turn].username);
+        }
+        // io.to(room).emit('score', {username: username, result: res, enter: arr.join('')}); //전광판
+      } else {
+        socket.emit('message', `당신차례가 아닙니다`)
+      }
+    } catch(err) {
+      console.log(err)
     }
   })
 
   // Runs when client disconnects
   socket.on('disconnect', () => {
     const user = userLeave(socket.id);
-    // console.log(user);
     const users = getRoomUsers(user.room);
+    
     let rival;
-
     if(users.length === 1){
+      if (users[0].username === 'COMPUTER') userLeave(socket.id);
       if(users[0].username && (users[0].username === user.username)){
         rival = users[1].username;
       } else {
@@ -182,8 +186,8 @@ io.on('connection', socket => {
       }
       // io.to(user.room).emit('end', rival);
       endAll(rival, user.username, user.room, io);
-
     }
+
     if (user) {
       io.to(user.room).emit('message', `${user.username}님이 나가셨습니다`);
       io.to(user.room).emit('roomUsers', {

16b15d64465116088a9fb89beb5121fe1f0782f4
chanul94 <cksdnfwkd@naver.com>
2020-09-02 16:13:40 +0900

    "url.js"_Fix urls

--- url.js
+++ url.js
@@ -1,9 +1,9 @@
 
-let web_server_url = 'http://3.34.178.78:3001'
-let bid_server_url = 'http://15.164.170.101:3005'
-let num_server_url = 'http://3.35.37.171:3006'
-let mole_server_url = 'http://13.125.22.16:3009'
-let chat_server_url = 'http://13.125.163.76:3002'
+let web_server_url = 'http://3.35.27.36:3001'
+let bid_server_url = 'http://3.35.27.36:3005'
+let num_server_url = 'http://3.35.27.36:3006'
+let mole_server_url = 'http://3.35.27.36:3009'
+let chat_server_url = 'http://3.35.27.36:3002'
 let client = 'http://project-2p.s3-website.ap-northeast-2.amazonaws.com'
 
 
